a68594b50bd1dcb712b4d0a438d674534c05230c
Ida Mäkelä <idamakela98@gmail.com>
2023-06-02 13:15:41 +0200

    started functions for client fetching to supabase

--- api-routes/posts.js
+++ api-routes/posts.js
@@ -1,15 +1,33 @@
-export const getPosts = () => {
-  //Handle get all posts
+import supabase from '../lib/supabaseClient';
+
+export const getPosts = async () => {
+    //Handle get all posts
+    const { data, error, status } = await supabase.from('Posts').select('*');
+
+    // if (error) {
+    //     console.log(error);
+    // }
+
+    return { data, error, status };
 };
 
-export const addPost = () => {
-  //Handle add post here
+export const addPost = async (post) => {
+    //Handle add post here
+    const { error, status } = await supabase.from('Posts').insert({ post });
+
+    return { error, status };
 };
 
-export const removePost = () => {
-  //Handle remove post here
+export const removePost = async (id) => {
+    //Handle remove post here
+    const { error, status } = await supabase
+        .from('Posts')
+        .delete()
+        .eq('id', id);
+
+    return { error, status };
 };
 
 export const editPost = () => {
-  //Handle edit post here
+    //Handle edit post here
 };
