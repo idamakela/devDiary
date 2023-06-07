9116f6977cad905a28c12659b36b9b93885a2bbd
idamakela <idamakela98@gmail.com>
2023-06-07 11:46:30 +0200

    supabase auth started

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,52 +1,90 @@
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-import styles from "./sidebar.module.css";
-import classNames from "classnames";
-
-const navItems = {
-  "/": {
-    name: "Home",
-  },
-  "/about": {
-    name: "About",
-  },
-  "/blog": {
-    name: "Blog",
-  },
-  "/create-post": {
-    name: "Create post",
-  },
-  "/login": {
-    name: "Login",
-  },
-};
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import styles from './sidebar.module.css';
+import classNames from 'classnames';
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
-  let pathname = usePathname() || "/";
-  if (pathname.includes("/blog/")) {
-    pathname = "/blog";
+  const supabaseClient = useSupabaseClient();
+  const user = useUser();
+  const router = useRouter();
+
+  let pathname = usePathname() || '/';
+  if (pathname.includes('/blog/')) {
+    pathname = '/blog';
   }
+
+  const navItems = {
+    '/': {
+      name: 'Home',
+    },
+    '/about': {
+      name: 'About',
+    },
+    '/blog': {
+      name: 'Blog',
+    },
+    '/create-post': {
+      name: 'Create post',
+      requiresAuth: true,
+    },
+    '/login': {
+      name: 'Login',
+      requiresAuth: false,
+    },
+    '/logout': {
+      name: 'Logout',
+      requiresAuth: true,
+      onClick: async () => {
+        await supabaseClient.auth.signOut();
+        router.push('/login') //sends user to login after sign out 
+      },
+    },
+  };
 
   return (
     <aside className={styles.container}>
       <div className={styles.sticky}>
-        <nav className={styles.navigation} id="nav">
+        <nav className={styles.navigation} id='nav'>
           <div className={styles.navigationItemWrapper}>
-            {Object.entries(navItems).map(([path, { name }]) => {
-              const isActive = path === pathname;
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className={classNames(styles.navigationItem, {
-                    [styles.textNeutral]: !isActive,
-                    [styles.fontBold]: isActive,
-                  })}
-                >
-                  <span className={styles.linkName}>{name}</span>
-                </Link>
-              );
-            })}
+            {Object.entries(navItems).map(
+              ([path, { name, requiresAuth, onClick }]) => {
+                const isActive = path === pathname;
+
+                if ((requiresAuth && !user) || (path === '/login' && user)) {
+                  return null;
+                }
+
+                if (path === '/logout') {
+                  return (
+                    <button
+                      key={path}
+                      className={classNames(styles.navigationButton, {
+                        [styles.textNeutral]: !isActive,
+                        [styles.fontBold]: isActive,
+                      })}
+                      onClick={onClick}
+                    >
+                      {name}
+                    </button>
+                  );
+                }
+
+                return (
+                  <Link
+                    key={path}
+                    href={path}
+                    className={classNames(styles.navigationItem, {
+                      [styles.textNeutral]: !isActive,
+                      [styles.fontBold]: isActive,
+                    })}
+                  >
+                    <span className={styles.linkName}>{name}</span>
+                  </Link>
+                );
+              }
+            )}
           </div>
         </nav>
       </div>
