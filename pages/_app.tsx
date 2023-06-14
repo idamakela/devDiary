28894568d062e9f60d27abbfb2a7f0236af41b62
idamakela <idamakela98@gmail.com>
2023-06-14 17:35:55 +0200

    still on rls

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,9 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import RootLayout from '../components/root-layout';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { useState } from 'react';
-
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
-import { supabase } from '@lib/supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 
 export default function App({ Component, pageProps }: AppProps) {
-  //const [supabaseClient] = useState(() => createPagesBrowserClient());
-
   return (
     <>
       <SessionContextProvider
