c8d26dcf77aba35f18361fa3a7ecadb73d204a82
Ida Mäkelä <idamakela98@gmail.com>
2023-06-24 14:15:03 +0200

    included error handling for date manipulation but now it dont wanna display the date...?

--- utils/manipulateDate.js
+++ utils/manipulateDate.js
@@ -1,7 +1,12 @@
-import { format } from 'date-fns';
+import { format, isValid } from 'date-fns';
 
 export const manipulateDate = (date) => {
   const createdAt = new Date(date);
+
+  if (!isValid(date)) {
+    return '';
+  }
+  
   const formatDate = format(createdAt, 'yyyy-MM-dd HH:mm');
 
   return formatDate;
