070e952e86f60466ac3b92a1fccb801ff1390332
Ida Mäkelä <idamakela98@gmail.com>
2023-06-24 19:30:13 +0200

    added content for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-import Heading from "@components/heading";
+import Heading from '@components/heading';
+import styles from './home.module.css';
 
 export default function Home() {
-  return <Heading>Home</Heading>;
+  return (
+    <>
+      <Heading>Home</Heading>
+      <p className={styles.text}>Welcome to devDiary!</p>
+      <p className={styles.text}>
+        Are you a curious developer with thoughts to share? Join our vibrant
+        community on devDiary and embark on an exploration of the captivating
+        world of web development. Dive into our short and insightful posts that
+        offer a glimpse into the minds of fellow coders. With the power of
+        Next.js, SWR, and Supabase, we provide a seamless browsing experience,
+        real-time updates, and secure data storage. Get inspired, gain
+        knowledge, and connect with like-minded individuals. Unleash your
+        thoughts and start sharing your unique perspective on devDiary today!
+      </p>
+    </>
+  );
 }
