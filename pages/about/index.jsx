49a9aa8912ec23df8a4cdf798976bc71aad16f3d
Ida Mäkelä <idamakela98@gmail.com>
2023-06-24 19:51:27 +0200

    made content for about page

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,7 +1,29 @@
-import Heading from "@components/heading";
+import Heading from '@components/heading';
 
 export default function About() {
-  return <Heading>About</Heading>;
+  return (
+    <>
+      <Heading>About</Heading>
+      <p>
+        devDiary is a personal project created by a junior frontend developer
+        based in Stockholm. Inspired by a course called Databases and
+        Webservers, this website serves as a documentation of the developer's
+        journey in the world of web development. Through devDiary, you'll find a
+        collection of short and insightful posts exploring various aspects of
+        web development. Built using Next.js, SWR, and Supabase, the website
+        offers a seamless user experience, real-time updates, and secure data
+        storage. Join us as we delve into the intricacies of web development and
+        gain insights into the life of a junior frontend developer. Whether
+        you're a fellow coder or simply curious about technology, devDiary
+        provides valuable knowledge and inspiration. Thank you for visiting
+        devDiary, and we hope you enjoy exploring our web development
+        adventures!
+      </p>
+      <p>Goodbye from devDiary!</p>
+      <p>Explore, engage, and keep coding!</p>
+      <p>
+        - <a href='https://idamakela.vercel.app/'>@Ida Mäkelä</a>
+      </p>
+    </>
+  );
 }
-
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import Heading from '@components/heading';
-import styles from './home.module.css';
 
 export default function Home() {
   return (
     <>
       <Heading>Home</Heading>
-      <p className={styles.text}>Welcome to devDiary!</p>
-      <p className={styles.text}>
+      <p>Welcome to devDiary!</p>
+      <p>
         Are you a curious developer with thoughts to share? Join our vibrant
         community on devDiary and embark on an exploration of the captivating
         world of web development. Dive into our short and insightful posts that
