ea2d4efa3662deecb28affdbc573d27913e20dc8
Ida Mäkelä <idamakela98@gmail.com>
2023-06-24 14:19:13 +0200

    found the date manipulation error... (wrong variable checked...)

--- utils/manipulateDate.js
+++ utils/manipulateDate.js
@@ -3,10 +3,10 @@ import { format, isValid } from 'date-fns';
 export const manipulateDate = (date) => {
   const createdAt = new Date(date);
 
-  if (!isValid(date)) {
+  if (!isValid(createdAt)) {
     return '';
   }
-  
+
   const formatDate = format(createdAt, 'yyyy-MM-dd HH:mm');
 
   return formatDate;
